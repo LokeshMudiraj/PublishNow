36710437db2240aceca31b58ce165a933f746f5c
MANAPATI LOKESH <lokeshmudiraj222@gmail.com>
2025-07-21 17:57:48 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
           ))}
         </div>
       </div>
-      <p className='py-4 text-center text-sm md:text-base text-gray-500/90'>Copyright 2025 QuickBlog - All Rights Reserved</p>
+      <p className='py-4 text-center text-sm md:text-base text-gray-500/90'>Copyright 2025 PublishNow - All Rights Reserved</p>
     </div>
   )
 }
